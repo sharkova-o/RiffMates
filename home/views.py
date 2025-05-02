4ae3ca38837c2bc2cfde306c5b540edfcc1e3aad
sharkova-o <sharkova-o@bk.ru>
2025-05-02 11:37:14 +0700

    add news page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 def credits(request):
     content = "Nicky\nOlesya"
     return HttpResponse(content, content_type="text/plain")
+
+def news(request):
+    data ={"news": [
+        "RiffMates now has a new page!",
+        "RiffMates has its first page"
+    ]}
+    return render(request, "news.html", data)
